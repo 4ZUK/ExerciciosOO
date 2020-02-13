56dc03fcf54a2569e4efba0ffd30848fbb75b96d
4ZUK <60348334+4zuk@users.noreply.github.com>
2020-02-13 14:48:52 -0300

    Exercicio 2 Iniciando

--- src/exercicio02/Triangulo.java
+++ src/exercicio02/Triangulo.java
@@ -1,4 +1,34 @@
 package exercicio02;
-
 public class Triangulo {
+    public int base;
+    public int altura;
+
+    public Triangulo(int base, int altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+
+    public int getBase() {
+        return base;
+    }
+
+    public void setBase(int base) {
+        this.base = base;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public double area(){
+        double resultado = (this.base * this.altura)/2;
+        System.out.println(resultado);
+        return resultado;
+    }
+
 }
