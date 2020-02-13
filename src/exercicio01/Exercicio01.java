cc2bdf03a8d5a2554bca669bad62c60e91bb1bb9
4ZUK <60348334+4ZUK@users.noreply.github.com>
2020-02-13 12:54:41 -0300

    Exercicio 1 ok

---
+++ src/exercicio01/Exercicio01.java
@@ -0,0 +1,13 @@
+package exercicio01;
+public class Exercicio01 {
+    public static void main(String[] args) {
+        Pessoa patrick = new Pessoa();
+
+        patrick.seuNome();
+        patrick.seuSobrenome();
+        patrick.suaIdade();
+
+        System.out.println(patrick);
+    }
+
+    }
---
+++ src/exercicio01/Pessoa.java
@@ -0,0 +1,19 @@
+package exercicio01;
+
+public class Pessoa {
+    public String nome;
+    public int idade;
+    public String sobrenome;
+
+    public void seuNome() {
+        System.out.println("Patrick");
+    }
+
+    public void suaIdade() {
+        System.out.println(22);
+    }
+
+    public void seuSobrenome() {
+        System.out.println("Doria");
+    }
+}
