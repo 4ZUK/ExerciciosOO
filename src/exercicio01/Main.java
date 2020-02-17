34bdfc47ff74a56ede3af7eef2393da769fcd72a
4ZUK <60348334+4ZUK@users.noreply.github.com>
2020-02-17 20:55:42 -0300

    Exercicio 1 ok!

---
+++ src/exercicio01/Main.java
@@ -0,0 +1,18 @@
+package exercicio01;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Tucano Coradini = new Tucano();
+
+
+        Coradini.voa = "sim";
+        Coradini.Voar();
+        Coradini.cor = "amarelado";
+        Coradini.Colorido();
+        Coradini.bico = "grande";
+        Coradini.Bicando();
+
+    }
+}
---
+++ src/exercicio01/Tucano.java
@@ -0,0 +1,21 @@
+package exercicio01;
+
+public class Tucano {
+    public String voa;
+    public String bico;
+    public String cor;
+
+
+    public void Voar() {
+        System.out.println("Estou voando muito alto");
+    }
+
+    public void Bicando() {
+        System.out.println("Eu tenho o bico maior de todos");
+    }
+
+    public void Colorido() {
+        System.out.println("Minhas penas sao as mais coloridas");
+    }
+
+}
