53680213d4d767c73560bf7b2ec47a90d958185c
4ZUK <60348334+4ZUK@users.noreply.github.com>
2020-02-18 00:00:27 -0300

    Exercicio 1 e 2 INICIANDO O 3

--- src/exercicio03/Livro.java
+++ src/exercicio03/Livro.java
@@ -1,4 +1,42 @@
 package exercicio03;
 
 public class Livro {
+
+    public String autor;
+    public String titulo;
+    public int numPagina;
+
+    public Livro(String autor, String titulo, int numPagina) {
+        this.autor = autor;
+        this.titulo = titulo;
+        this.numPagina = numPagina;
+    }
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getNumPagina() {
+        return numPagina;
+    }
+
+    public void setNumPagina(int numPagina) {
+        this.numPagina = numPagina;
+    }
 }
+
+
+
+
+
